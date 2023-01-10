42c2805979d083b4b77802bed74e39580b73747c
yayobyte <yayobyte@gmail.com>
2023-01-10 15:52:37 +0400

    Form Colors

--- components/PlaceForm.tsx
+++ components/PlaceForm.tsx
@@ -1,9 +1,40 @@
-import {View, Text} from "react-native";
+import { View, Text, ScrollView, TextInput, StyleSheet } from "react-native";
+import { useState } from "react";
+import { Colors } from "../constants/colors";
 
 export const PlaceForm = () => {
+    const [enteredTitle, setEnteredTitle] = useState('')
+    
+    const changeTitleHandler = (enteredText: string) => {
+        setEnteredTitle(enteredText)
+    }
     return (
-        <View>
-            <Text>The place form</Text>
-        </View>
+        <ScrollView style={style.form}>
+            <View>
+                <Text style={style.label}>Title</Text>
+                <TextInput onChangeText={changeTitleHandler} value={enteredTitle} style={style.input}/>
+            </View>
+        </ScrollView>
     )
 }
+
+const style = StyleSheet.create({
+    form: {
+        flex: 1,
+        padding: 24
+    },
+    label: {
+        fontWeight: 'bold',
+        marginBottom: 4,
+        color: Colors.primary500,
+    },
+    input: {
+        marginVertical: 8,
+        paddingHorizontal: 4,
+        paddingVertical: 8,
+        fontSize: 16,
+        borderBottomColor: Colors.primary700,
+        borderBottomWidth: 2,
+        backgroundColor: Colors.primary100,
+    },
+})
